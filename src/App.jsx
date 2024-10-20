5c27a661d9e1c33057ddce0591e3a10f6b19dc47
JhoamSebastianMunoz <jhoamsebastian68@gmail.com>
2024-10-19 20:12:09 -0500

    se crea el componente TaskInput

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { TaskInput } from './Components/taskInput/TaskInput'
+
 import './App.css'
 
 export function App() {
@@ -6,7 +8,7 @@ export function App() {
 
   return (
     <>
-      <h1>Yo soy app</h1>
+      <TaskInput/>
     </>
   )
 }
