52eaa87dfa284dd3d4901b3b7f8d788b1ed0332e
JhoamSebastianMunoz <jhoamsebastian68@gmail.com>
2024-10-19 19:40:34 -0500

    creacion del proyecto base

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Yo soy app</h1>
+    </>
+  )
+}
+
+
